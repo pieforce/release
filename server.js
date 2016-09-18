d866029dc529eab42f82e8a746ba465d93805241
pieforce <wing3000@gmail.com>
2016-09-17 23:27:03 -0700

    Modified port for heroku use

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/:name', route.dispHome);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(port);
+console.log('Listening on port ' + port + '...');
